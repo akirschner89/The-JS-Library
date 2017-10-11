3581426f3e6493031f8f762d49aa78815746ca95
Adam Kirschner <>
2017-10-10 20:24:56 -0500

    first commit

---
+++ ES6-2015.js
@@ -0,0 +1,35 @@
+// let
+// Const
+// spread operator
+// destructoring
+// resturctoring
+// arrow functions =>
+// template strings
+
+//===Object Oriented Programming===
+
+// * Class keyword *
+// reserved keyword in javascript - involved with constructors & prototypes
+// does not hoist!
+// called an instance method, each class creates a new instance
+
+// class Student {
+//     constructor(first, last) {
+//         this.first = first;
+//         this.last = last;
+//     }
+// }
+
+// var Rocky = new Student(Rocky, Balboa);
+// place prototype methods inside the class
+// static keyword also can use static methods - don't have to use for every method
+
+// class Student {
+//     constructor(first, last) {
+//         this.first = first;
+//         this.last = last;
+//     }
+//      sayHi() {
+//      return `Hey I'm ${this.first} ${this.last}`
+//      }
+// }
---
+++ essentialJS.js
@@ -0,0 +1,35 @@
+//======General======
+// hoisting
+// difference between == and ===
+// typeof
+// ES6 =>
+
+//======Arrays========
+// splice
+// slice
+// concat
+// split
+// join
+// map
+// sort
+// reduce
+
+
+//=======Functions========
+// constructors
+// call
+// apply
+// bind
+// arguments
+// converting arguments into an actual array
+// memoization
+// switch
+// while loop
+// do while loop
+// asynchronous flow
+// promises
+// Promise.all
+// promise reject / resolve
+// event delegation
+// recursive functions
+// how to make an AJAX request
